32efe969e741613bcbd6d1ae5918dafc39f10321
TejashreyaD <tejashreyad@gmail.com>
2025-10-05 16:22:46 -0400

    yes

--- polynomial.py
+++ polynomial.py
@@ -3,11 +3,34 @@ def evaluate_polynomial(degree, x, constant_term, *coefficients):
         raise ValueError(f"Need {degree} coefficient(s)")
 
     total = constant_term
-    print (f"(value of the constant term)= {total}")
+    print (f"SO(value of the constant term)= {total}")
 
-    k=1
-    while k <= degree:
-        value = coefficients[k-1] * (x**k)
-        old_total = total
+    for k in range(1, degree +1):
+        value = coefficients[k-1] * x ** k
+        total_old= total
         total = total + value
-        print(f"S{k} (sum of the {k+1} lowest terms) = {old_total} + {coefficients[k-1]}({x}^{k}) = {total}")
+        print(f"S{k} (sum of the {k+1} lowest terms) = {total_old} + ({coefficients[k - 1]} × {x}^{k}) = {total}")
+
+def main():
+    while True:
+        degree = int(input("Degree of polynomial: "))
+        x = float(input("Value of x: "))
+        constant_term = float(input("Constant term: "))
+
+        coefficients = []
+        for k in range(1, degree + 1):
+            coefficients.append(float(input("Coefficient {k}: ")))
+
+            print("\n Evaliauting polynomical...\n")
+            final=evaluate_polynomial(degree, x, constant_term, *coefficients)
+            print(final)
+
+            except ValueError as e:
+            print(e)
+            continue
+
+            do_again = input("Do you want to continue?: ")
+            if again != "y":
+                break
+if __name__ == "__main__":
+    main()
